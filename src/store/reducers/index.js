735ccaee590daccb0c8af1145cf0d912a4eda958
Jahanzaib Tayyab <jahanzaib.tayyab@devigital.com>
2021-11-20 19:47:49 +0500

    WAK-13 generate unique url and pin code implementation and also implemet redux store for it

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import NotionAuth from "./NotionAuth";
+import Preparing from "./Preparing";
 
 const rootReducer = combineReducers({
   SignIn,
   SignUp,
   NotionAuth,
+  Preparing,
 });
 export default rootReducer;
---
+++ src/utils/helperFunctions.js
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from "uuid";
+import { generatedUrl } from "../constants/Common";
+import generator from "generate-password";
+
+const generateCode = () => {
+  const timestamp = Date.now();
+  const id = uuidv4();
+  const code = `${id}-${timestamp}`;
+  return code;
+};
+
+const generateUrl = () => {
+  return generatedUrl.LINK + generateCode();
+};
+
+const generatePinCode = () => {
+  return generator.generate({
+    length: 8,
+    numbers: true,
+    lowercase: true,
+  });
+};
+export { generateUrl, generatePinCode };
